8177344d59014203d6cb688deb1770bce5ffb8d3
Shashmit kumar <shaashmit@gmail.com>
2023-12-18 01:33:44 +0530

    tweak in apiRouterqq

--- server/model/User.js
+++ server/model/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: String,
   age: Number,
   selectedBatch: String,
